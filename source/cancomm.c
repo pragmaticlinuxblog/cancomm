414969ea310079a2b7b9ffc55b67b7cc0b156251
PragmaticLinux <info@pragmaticlinux.com>
2022-02-13 23:02:00 +0100

    Setup initial library API.

--- source/cancomm.c
+++ source/cancomm.c
@@ -12,6 +12,13 @@
 #include "cancomm.h"                        /* CAN communication header                */
 
 
+/****************************************************************************************
+* Macro definitions
+****************************************************************************************/
+/** \brief Maximum number of bytes in a CAN message. */
+#define CANCOMM_CAN_DATA_LEN_MAX       (8U)
+
+
 /************************************************************************************//**
 ** \brief     Creates a new context for the CAN communication instance. All subsequent
 **            library functions need this context.
@@ -31,8 +38,8 @@ cancomm_t * cancomm_new(void)
 
 /************************************************************************************//**
 ** \brief     Releases the context. Should always be called for all CAN communication
-**            instances, with function cancomm_new(), once you no longer need it.
-** \param     Pointer to the CAN communication instance context.
+**            instances, created with function cancomm_new(), once you no longer need it.
+** \param     ctx Pointer to the CAN communication instance context.
 **
 ****************************************************************************************/
 void cancomm_free(cancomm_t * ctx)
@@ -46,6 +53,116 @@ void cancomm_free(cancomm_t * ctx)
     /* TODO Release allocated memory of the context. */
   }
 } /*** end of cancomm_free ***/
+
+
+/************************************************************************************//**
+** \brief     Connects to the specified SocketCAN device. Note that you can run command
+**            "ip addr" in the terminal to determine the SocketCAN device name known to
+**            your Linux system.
+** \param     ctx Pointer to the CAN communication instance context.
+** \param     device Null terminated string with the SocketCAN device name, e.g. "can0".
+** \return    CANCOMM_TRUE if successfully connected to the SocketCAN device.
+**            CANCOMM_FALSE otherwise.
+**
+****************************************************************************************/
+uint8_t cancomm_connect(cancomm_t * ctx, char const * device)
+{
+  uint8_t result = CANCOMM_FALSE;
+
+  /* Verify parameters. */
+  assert((ctx != NULL) && (device != NULL));
+
+  /* Only continue with a valid parameters. */
+  if ((ctx != NULL) && (device != NULL))
+  {
+    /* TODO Implement cancomm_connect(). */
+  }
+
+  /* Give the result back to the caller. */
+  return result;
+} /*** end of cancomm_connect ***/
+
+
+/************************************************************************************//**
+** \brief     Disconnects from the SocketCAN device.
+** \param     ctx Pointer to the CAN communication instance context.
+**
+****************************************************************************************/
+void cancomm_disconnect(cancomm_t * ctx)
+{
+  /* Verify parameter. */
+  assert(ctx != NULL);
+
+  /* Only continue with a valid parameter. */
+  if (ctx != NULL)
+  {
+    /* TODO Implement cancomm_disconnect(). */
+  }
+} /*** end of cancomm_disconnect ***/
+
+
+/************************************************************************************//**
+** \brief     Submits a CAN message for transmission.
+** \param     ctx Pointer to the CAN communication instance context.
+** \param     id CAN message identifier.
+** \param     ext CANCOMM_FALSE for a 11-bit message identifier, CANCOMM_TRUE of 29-bit.
+** \param     len Number of CAN message data bytes.
+** \param     data Pointer to array with data bytes.
+** \return    CANCOMM_TRUE if successfully submitted the message for transmission.
+**            CANCOMM_FALSE otherwise.
+**
+****************************************************************************************/
+uint8_t cancomm_transmit(cancomm_t * ctx, uint32_t id, uint8_t ext, uint8_t len, 
+                         uint8_t const * data)
+{
+  uint8_t result = CANCOMM_FALSE;
+
+  /* Verify parameters. */
+  assert((ctx != NULL) && (len <= CANCOMM_CAN_DATA_LEN_MAX) && (data != NULL));
+
+  /* Only continue with a valid parameters. */
+  if ((ctx != NULL) && (len <= CANCOMM_CAN_DATA_LEN_MAX) && (data != NULL))
+  {
+    /* TODO Implement cancomm_transmit(). */
+  }
+
+  /* Give the result back to the caller. */
+  return result;
+} /*** end of cancomm_transmit ***/
+
+
+/************************************************************************************//**
+** \brief     Retrieves a possibly received CAN message.
+** \param     ctx Pointer to the CAN communication instance context.
+** \param     id Pointer to where the CAN message identifier is stored.
+** \param     ext Pointer to where the CAN identifer type is stored. CANCOMM_FALSE for a
+**            11-bit message identifier, CANCOMM_TRUE of 29-bit.
+** \param     len Pointer to where the number of CAN message data bytes is stored.
+** \param     data Pointer to array where the data bytes are stored.
+** \param     timestamp Pointer to where the timestamp of the message is stored.
+** \return    CANCOMM_TRUE if a new message was received and copied. CANCOMM_FALSE 
+**            otherwise.
+**
+****************************************************************************************/
+uint8_t cancomm_receive(cancomm_t * ctx, uint32_t * id, uint8_t * ext, uint8_t * len, 
+                        uint8_t * data, uint32_t * timestamp)
+{
+  uint8_t result = CANCOMM_FALSE;
+
+  /* Verify parameters. */
+  assert((ctx != NULL) && (id != NULL) && (ext != NULL) && (len != NULL) && 
+        (data != NULL) && (timestamp != NULL));
+
+  /* Only continue with a valid parameters. */
+  if ((ctx != NULL) && (id != NULL) && (ext != NULL) && (len != NULL) && 
+      (data != NULL) && (timestamp != NULL))
+  {
+    /* TODO Implement cancomm_receive(). */
+  }
+
+  /* Give the result back to the caller. */
+  return result;
+} /*** end of cancomm_receive ***/
 
 
 /*********************************** end of cancomm.c **********************************/
--- source/cancomm.h
+++ source/cancomm.h
@@ -20,10 +20,13 @@ extern "C" {
 * Macro definitions
 ****************************************************************************************/
 /** \brief Boolean true value. */
-#define CANCOMM_TRUE         (1U)
+#define CANCOMM_TRUE                   (1U)
 
 /** \brief Boolean false value. */
-#define CANCOMM_FALSE        (0U)
+#define CANCOMM_FALSE                  (0U)
+
+/** \brief Maximum number of bytes in a CAN message. */
+#define CANCOMM_CAN_DATA_LEN_MAX       (8U)
 
 
 /****************************************************************************************
@@ -40,8 +43,15 @@ typedef struct
 /****************************************************************************************
 * Function prototypes
 ****************************************************************************************/
+/* TODO Perhaps add API to enumerate list of CAN devices. */
 cancomm_t * cancomm_new(void);
 void        cancomm_free(cancomm_t * ctx);
+uint8_t     cancomm_connect(cancomm_t * ctx, char const * device);
+void        cancomm_disconnect(cancomm_t * ctx);
+uint8_t     cancomm_transmit(cancomm_t * ctx, uint32_t id, uint8_t ext, uint8_t len, 
+                             uint8_t const * data);
+uint8_t     cancomm_receive(cancomm_t * ctx, uint32_t * id, uint8_t * ext, uint8_t * len, 
+                             uint8_t * data, uint32_t * timestamp);
 
 
 #ifdef __cplusplus
